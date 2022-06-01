5faa36592405e32c836809c56e80779cb4d360fd
Antonio Mata <antonio.mata.sv@gmail.com>
2022-05-31 21:19:36 -0600

    add cjs files

--- CJS/another-file.js
+++ CJS/another-file.js
@@ -1,1 +1,7 @@
-module.exports = () => new Date().toDateString()
+module.exports = () => new Date().toISOString()
+
+// if (require.main === module) {
+//     console.log(`I was called as main module: ${new Date().toDateString()}`)
+// } else {
+//     module.exports = () => `I was called as a dependency: ${new Date().toISOString()}`
+// }
