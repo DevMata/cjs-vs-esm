ee3d3c584465d72996955569336f01b6e7ee5c6e
Antonio Mata <antonio.mata.sv@gmail.com>
2022-06-02 13:24:39 -0600

    update another-file.js for ESM

--- ESM/another-file.js
+++ ESM/another-file.js
@@ -1,3 +1,5 @@
-const ciao = require('./a-cjs-file.cjs')
+// const ciao = require('./a-cjs-file.cjs')
+// export default (name) => `Hello ${name}. ${ciao('Antonio')}`
 
-export default (name) => `Hello ${name}. ${ciao('Antonio')}`
+export default (name) => `Hello ${name}.`
+
